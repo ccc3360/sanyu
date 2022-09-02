c8146dfcf3fd8970c4d7ead876fb210faca311f0
hhh <3360746040@qq.com>
2022-09-02 17:53:25 +0800

    开发环境搭建，前端资源导入

---
+++ sanyu_take_out/src/main/java/com/example/SanyuApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@Slf4j
+@SpringBootApplication
+public class SanyuApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SanyuApplication.class,args);
+    }
+}
